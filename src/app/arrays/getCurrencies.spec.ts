511516ea8c15b7a4c71e994e345ad6c8acba5a9c
WarGooglee <s00227303@mail.itsligo.ie>
2024-01-23 12:27:12 +0000

    Week 1 Lab Complete

--- src/app/arrays/getCurrencies.spec.ts
+++ src/app/arrays/getCurrencies.spec.ts
@@ -4,13 +4,7 @@ describe ('getCurrencies', () => {
     it('should contain the USD in the array', () => {
         const result = getCurrencies();
         expect(result).toContain('USD');
-    });
-    it('should contain the GBP in the array', () => {
-        const result = getCurrencies();
         expect(result).toContain('GBP');
-    });
-    it('should contain the EUR in the array', () => {
-        const result = getCurrencies();
         expect(result).toContain('EUR');
     });
     it('should not contain CAD in the array', () => {
